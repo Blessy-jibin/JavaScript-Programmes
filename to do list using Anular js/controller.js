612bb5fd5cd55188a12243b40975663d77598841
Blessy-jibin <blessy.xavier31@gmail.com>
2018-03-02 10:35:26 +0100

    Adding full js

--- to do list using Anular js/controller.js
+++ to do list using Anular js/controller.js
@@ -4,14 +4,33 @@ app.controller("myCtrl", function($scope) {
 
 	$scope.todolist=[];
 	 
+   $scope.AddContacts=function(){
+   add=1;
+   $scope.listitem=" ";
+   console.log("add="+add);
 
+   }
 
     $scope.sve=function(){
-    $scope.todolist.push($scope.listitem);
-    
-    console.log($scope.todolist);
-    
+      if(add==1){
+        $scope.todolist.push($scope.listitem);
+        console.log($scope.todolist);
+        
+      }
+     else{
+     	
+    	$scope.todolist[index]=$scope.listitem;
+     	
+     }
 	}
 
+
+$scope.update=function(item){
+ add=0;
+console.log(item);
+$scope.listitem=item;
+index = $scope.todolist.indexOf(item);
+console.log("add=" + add);
+}
 
 });
